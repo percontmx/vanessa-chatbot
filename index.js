a119121e86690d12808f5a989a898646da5c1f33
Alex Percont <alexpercont@gmail.com>
2020-07-19 11:53:35 -0500

    Configuración de ESLint.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-'use scrict'
+'use scrict';
 
-const functions = require('firebase-functions')
-const { WebhookClient } = require('dialogflow-fulfillment')
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
 
-exports.vanessa_chatbot_functions = functions.https.onRequest((request, response) => {
-    const agent = new WebhookClient({ request, response })
-    let intent = new Map()
-    agent.handleRequest(intent)  
-})
+exports.vanessa_chatbot_functions =
+    functions.https.onRequest((request, response) => {
+      const agent = new WebhookClient({request, response});
+      const intent = new Map();
+      agent.handleRequest(intent);
+    });
